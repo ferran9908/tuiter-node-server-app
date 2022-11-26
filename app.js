12d7d3349c9fb43e8497eec9fe2d35b23878b56b
Ferran <ferran.9908@gmail.com>
2022-11-26 11:22:58 -0800

    A9

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+import mongoose from "mongoose";
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/tuiter";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
